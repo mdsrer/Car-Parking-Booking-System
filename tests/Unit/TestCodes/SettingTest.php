170db2f8669b3cf5ac1b2ad8dbbe43748cf8d455
mdsrer <samiur.rahman@g.bracu.ac.bd>
2022-04-30 20:37:54 +0600

    TestCodes

---
+++ tests/Unit/TestCodes/SettingTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Unit;
+use App\Models\SettingDB;
+use PHPUnit\Framework\TestCase;
+
+class SettingTest extends TestCase{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+
+    //setting class has only protected function. So, there is no other test.
+    public function test_setting_test()
+    {
+        $this->assertTrue(true);
+    }
+}
